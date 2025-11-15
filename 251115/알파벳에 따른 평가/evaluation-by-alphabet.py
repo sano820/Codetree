e206a8cd68425705c5d53b90e0d9707115ce9d87
b.upline_00 <sano820@naver.com>
2025-11-15 16:16:13 +0900

    Title: 알파벳에 따른 평가, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251115/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,14 @@
+st = input()
+
+if st == 'S':
+    print("Superior")
+elif st == 'A':
+    print("Excellent")
+elif st == 'B':
+    print("Good")
+elif st == 'C':
+    print("Usually")
+elif st == 'D':
+    print('Effort')
+else:
+    print('Failure')

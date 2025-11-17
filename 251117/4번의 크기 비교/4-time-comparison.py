bc9201dc89b9ef5c03c043488f0e8bb5b2d445f5
b.upline_00 <sano820@naver.com>
2025-11-17 21:12:25 +0900

    Title: 4번의 크기 비교, Time: 81ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 251117/4번의 크기 비교/4-time-comparison.py
@@ -0,0 +1,23 @@
+A = int(input())
+B,C,D,E = map(int, input().split())
+
+if A>B:
+    print(a)
+else:
+    print(0)
+
+if A>C:
+    print(a)
+else:
+    print(0)
+
+if A>C:
+    print(a)
+else:
+    print(0)
+
+if A>E:
+    print(a)
+else:
+    print(0)
+

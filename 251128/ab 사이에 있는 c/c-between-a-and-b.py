6d3cd6764fd23f344d78f6e8f4bdedf112f89ebd
b.upline_00 <sano820@naver.com>
2025-11-28 18:15:17 +0900

    Title: ab 사이에 있는 c, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251128/ab 사이에 있는 c/c-between-a-and-b.py
@@ -0,0 +1,12 @@
+a,b,c = map(int, input().split())
+
+answer = False
+
+for i in range(a,b+1):
+    if i % c ==0:
+        answer = True
+
+if answer == True:
+    print("YES")
+else:
+    print("NO")

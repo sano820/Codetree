9c6dabcb8b7023160b3008b0cfe8e8e44b331110
b.upline_00 <sano820@naver.com>
2026-01-05 16:07:35 +0900

    Title: 함수를 이용한 합과 소수 판별, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 260105/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.py
+++ 260105/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.py
@@ -1,3 +1,4 @@
+import math
 a, b = map(int, input().split())
 
 # Please write your code here.
@@ -11,3 +12,10 @@ def is_prime(n: int) -> bool:
 
 def digit_sum_even(n: int) -> bool:
     return sum(int(c) for c in str(n)) % 2 == 0 
+
+count = 0
+for i in range(a,b+1):
+    if is_prime(i):
+        if digit_sum_even(i):
+            count += 1
+print(count)

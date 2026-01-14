79c2914a1428d6e07894587a171585eaaa74b797
b.upline_00 <sano820@naver.com>
2026-01-14 17:12:03 +0900

    Title: 재귀함수의 꽃, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260114/재귀함수의 꽃/the-flower-of-the-recursive-function.py
@@ -0,0 +1,12 @@
+N = int(input())
+
+# Please write your code here.
+def sol(n):
+    n -= 1
+    if n == -1:
+        return
+    print(n+1, end = ' ')
+    sol(n)
+    print(n+1, end = ' ')
+
+sol(N)
